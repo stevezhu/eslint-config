c0ad17d78416c7c8820c4c2e441d39cc973d6b89
Steve Zhu <4130171+stevezhu@users.noreply.github.com>
2025-08-25 15:27:16 -0400

    Switch to using eslint-plugin-better-tailwindcss

--- src/tailwind.js
+++ src/tailwind.js
@@ -1,4 +1,13 @@
-import tailwind from 'eslint-plugin-tailwindcss';
+import eslintPluginBetterTailwindcss from 'eslint-plugin-better-tailwindcss';
 import tseslint from 'typescript-eslint';
 
-export default tseslint.config(...tailwind.configs['flat/recommended']);
+export default tseslint.config({
+  files: ['**/*.{jsx,tsx}'],
+  plugins: {
+    'better-tailwindcss': eslintPluginBetterTailwindcss,
+  },
+  rules: {
+    ...eslintPluginBetterTailwindcss.configs['recommended-error'].rules,
+    'better-tailwindcss/no-deprecated-classes': 'warn',
+  },
+});
