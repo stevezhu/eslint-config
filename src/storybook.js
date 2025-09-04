b4718a7e5dbb944db6c428e3e21c0d7c2514dc77
Steve Zhu <4130171+stevezhu@users.noreply.github.com>
2025-09-03 21:28:10 -0400

    Storybook eslint issue seems fixed

--- src/storybook.js
+++ src/storybook.js
@@ -1,6 +1,4 @@
 import { defineConfig } from 'eslint/config';
-// TODO: add dist import because package.json of this package exports the cjs build
-// check if this is fixed once https://github.com/storybookjs/storybook/pull/32064 lands in storybook 10
-import storybook from 'eslint-plugin-storybook/dist';
+import storybook from 'eslint-plugin-storybook';
 
 export default defineConfig(...storybook.configs['flat/recommended']);
