baeb4d4b71f9671778e01d2431c06e83a8549c82
Steve Zhu <4130171+stevezhu@users.noreply.github.com>
2025-09-05 13:52:11 -0400

    Fix storybook config

--- src/storybook.ts
+++ src/storybook.ts
@@ -1,4 +1,4 @@
 import { defineConfig } from 'eslint/config';
 import storybook from 'eslint-plugin-storybook';
 
-export default defineConfig(storybook.default.configs['flat/recommended']);
+export default defineConfig(storybook.configs['flat/recommended']);
