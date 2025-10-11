74e21e4d3f5451cf93f34703c86bda7f2e600c86
Steve Zhu <4130171+stevezhu@users.noreply.github.com>
2025-10-11 14:55:05 -0400

    Fix expo config and add tests

--- src/configs/import.ts
+++ src/configs/import.ts
@@ -2,31 +2,37 @@ import { defineConfig } from 'eslint/config';
 import importPlugin from 'eslint-plugin-import';
 import simpleImportSort from 'eslint-plugin-simple-import-sort';
 
+export const importConfig = defineConfig({
+  name: 'import/custom',
+  rules: {
+    'import/enforce-node-protocol-usage': ['error', 'always'],
+    'import/first': 'error',
+    'import/newline-after-import': 'error',
+  },
+});
+
+export const simpleImportSortConfig = defineConfig({
+  name: 'simple-import-sort/custom',
+  plugins: {
+    'simple-import-sort': simpleImportSort,
+  },
+  rules: {
+    'simple-import-sort/imports': 'error',
+    'simple-import-sort/exports': 'error',
+  },
+});
+
 export default defineConfig(
   importPlugin.flatConfigs.recommended,
   {
-    name: 'import/custom',
+    name: 'import/settings',
     settings: {
       'import/resolver': {
         typescript: true,
         node: true,
       },
     },
-    rules: {
-      'import/enforce-node-protocol-usage': ['error', 'always'],
-      'import/first': 'error',
-      'import/newline-after-import': 'error',
-    },
   },
-
-  {
-    name: 'simple-import-sort/custom',
-    plugins: {
-      'simple-import-sort': simpleImportSort,
-    },
-    rules: {
-      'simple-import-sort/imports': 'error',
-      'simple-import-sort/exports': 'error',
-    },
-  },
+  importConfig,
+  simpleImportSortConfig,
 );
