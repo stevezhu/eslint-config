0a0cff0dc29f86a2840468a6a69e2d0dede20212
Steve Zhu <4130171+stevezhu@users.noreply.github.com>
2025-10-28 19:17:01 -0400

    chore(lint): add vitest to eslint config (#98)

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,5 +1,10 @@
 import { defineConfig, globalIgnores } from 'eslint/config';
 
 import ts from './src/ts.js';
+import vitest from './src/vitest.js';
 
-export default defineConfig(globalIgnores(['dist/']), ts.configs.recommended);
+export default defineConfig(
+  globalIgnores(['dist/']),
+  ts.configs.recommended,
+  vitest.configs.recommended,
+);
